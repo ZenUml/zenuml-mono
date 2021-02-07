baaaf792b11a2191934fde6a3525c12e48e29e2d
Tomasz Waraksa <tomasz@waraksa.net>
2021-02-07 01:19:20 +0000

    Minor cleanup

--- packages/application/server/src/index.js
+++ packages/application/server/src/index.js
@@ -1,20 +1,26 @@
 import fastify from 'fastify'
 import fastifyCORS from 'fastify-cors'
-import fastifySwagger from 'fastify-swagger'
+import fastifyOpenAPI from 'fastify-swagger'
 import pkg from '../package.json'
 import routes from './routes'
 import getDocumentationOptions from './documentation'
 
-// Initialize fastify application
+// CONFIGURATION
+const host = 'localhost'
+const port = 3333
+
+// APPLICATION
 console.log('-'.repeat(50))
 console.log(`API Server ${pkg.name} v.${pkg.version}, ${new Date().toLocaleTimeString()}`)
 const app = fastify()
-// Enables requests from UI running on another domain
-app.register(fastifyCORS, { origin: '*' })
-// Activates OpenAPI documentation at /documentation route
-app.register(fastifySwagger, getDocumentationOptions('localhost', 3333, '/documentation'))
 
-// Mount API routes
+// PLUGINS
+// CORS: Enables requests from UI running on another domain
+app.register(fastifyCORS, { origin: '*' })
+// OpenAPI: Enables API documentation UI at /documentation route
+app.register(fastifyOpenAPI, getDocumentationOptions(host, port, '/documentation'))
+
+// ROUTES
 for (const route of routes) {
   console.log(`> ${route.method} ${route.url}`)
   app.route(route)
@@ -22,12 +28,11 @@ for (const route of routes) {
 console.log('> GET /documentation')
 console.log('-'.repeat(50))
 
-// Start listening
-const port = 3333
+// LISTEN
 const start = async () => {
   try {
     await app.listen(port)
-    console.log(`Listening on http://localhost:${port}/`)
+    console.log(`Listening on http://${host}:${port}/`)
 
   } catch (err) {
     console.error(err)
